ac1d81c6d8c7913c6605fe53971bbf0fdf2a7494
Andy Patari <andy.patari@gmail.com>
2024-03-30 11:34:03 -0400

    remove unneeded imports in app, move some to practice

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { make_blank, make_range } from './helpers';
-import { Button } from 'react-bootstrap';
-
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Layout from './Layout';
-
 import Practice from './Practice';
 import Ranges from './Ranges';
 
@@ -35,31 +31,3 @@ export default function App() {
 }
 
   
-
-  // const [cards, setCards] = useState([])
-  // const [deckId, setDeckId] = useState(null)
-
-  // useEffect(() => {
-  //   const range = make_range(['AK', 'AKs'])
-  //   console.log(Object.keys(range).length)
-  //   fetch('https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
-  //   .then(res => res.json())
-  //   .then(data => setDeckId(data.deck_id))
-  // }, [])
-
-  // const drawCard = () => {
-  
-    
-  //   fetch(`https://www.deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`)
-  //   .then(res => res.json())
-  //   .then(data => setCards(cards.concat(data.cards)))
-  // }
-
-
-
- 
-      // {/* {deckId}
-      // <Button onClick={drawCard} >Draw</Button>
-      // {cards.map(card => <p key={card.code} >{card.code}</p> )} */}
-
-   
--- src/Practice.js
+++ src/Practice.js
@@ -1,4 +1,8 @@
-import React from "react";
+
+import React, { useEffect, useState } from 'react';
+
+import { make_blank, make_range } from './helpers';
+import { Button } from 'react-bootstrap';
 
 function Practice() {
     return (
@@ -9,3 +13,31 @@ function Practice() {
 }
 
 export default Practice
+
+  // const [cards, setCards] = useState([])
+  // const [deckId, setDeckId] = useState(null)
+
+  // useEffect(() => {
+  //   const range = make_range(['AK', 'AKs'])
+  //   console.log(Object.keys(range).length)
+  //   fetch('https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
+  //   .then(res => res.json())
+  //   .then(data => setDeckId(data.deck_id))
+  // }, [])
+
+  // const drawCard = () => {
+  
+    
+  //   fetch(`https://www.deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`)
+  //   .then(res => res.json())
+  //   .then(data => setCards(cards.concat(data.cards)))
+  // }
+
+
+
+ 
+      // {/* {deckId}
+      // <Button onClick={drawCard} >Draw</Button>
+      // {cards.map(card => <p key={card.code} >{card.code}</p> )} */}
+
+   
