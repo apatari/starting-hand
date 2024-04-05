ac0621e57804a9a9b8ab24e5e6f438490974a076
Andy Patari <andy.patari@gmail.com>
2024-04-05 11:21:06 -0400

    add table for sample range, not yet visualized

--- src/Ranges.js
+++ src/Ranges.js
@@ -1,7 +1,34 @@
 import React  from "react";
+import { rangeRows } from "./range_visual";
+import { Table } from "react-bootstrap";
 
 export default function Ranges() {
     return (
-        <div>Ranges</div>
+        <div>Example UTG Range: 
+
+            <Table size="sm" variant="dark">
+                <tbody>
+                    {rangeRows.map(row => {
+                        return(
+                            <tr>
+                                {row.map(hand => {
+                                    
+                                        if(hand==="AA"){
+                                            return (<td className="bg-info" >{hand}</td>)
+                                        } else {
+                                            return (<td >{hand}</td>)
+                                        }
+                                      
+                                         
+                                    
+                                })}
+                            </tr>
+                        )
+                    })}
+                </tbody>
+            </Table>
+
+        </div>
+
     )
 }
